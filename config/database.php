8682e5b50985a3b2cf1539cc06c6b22a4ec00dc4
blotalt <baska7gzihkara71@gmail.com>
2026-07-20 11:48:55 +0700

    almost finish

--- config/database.php
+++ config/database.php
@@ -34,8 +34,8 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'charset' => 'utf8',
+'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
